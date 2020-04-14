599f5ea1a6ae721a4c4ed87b15774d66bdd7c1a3
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-04-14 04:05:59 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,7 +6,7 @@ sudo apt-add-repository ppa:ansible/ansible
 echo "<< Update >>"
 sudo apt update
 
-echo"<< Install ansible >>"
+echo "<< Install ansible >>"
 sudo apt install ansible
 
 echo "<< Now edit this file and put the ips in it >> type sudo nano /etc/ansible/hosts"
