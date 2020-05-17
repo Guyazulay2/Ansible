3981d4231acfae5e3bdfa82a419293f678e665f1
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-05-17 21:02:34 +0200

    Add files via upload

---
+++ ansible.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+
+while [ "true" == "true" ]
+do
+
+
+
+echo "
+(1) | Install ansible on this computer
+(2) | Install ansible on slave
+Your Choice :"
+read i
+
+
+if [ $i == "1" ]; then
+	sudo apt-get install software-properties-common -y
+	sudo apt-add-repository ppa:ansible/ansibl -y
+	sudo apt update -y
+	sudo apt install ansible -y
+	sudo apt-get install python3-pip -y
+
+
+elif [ $i == "2" ]; then
+	echo "Enter the slave IP :"
+	read IP
+	echo "Enter the Password :"
+	read PASS
+	sshpass -p 'rootroot' ssh root@$IP 'sudo apt-get install software-properties-common -y \\
+	sudo apt-add-repository ppa:ansible/ansibl -y \\
+	sudo apt update -y \\
+        sudo apt install ansible -y \\
+        sudo apt-get install python3-pip -y '
+
+else
+	echo "Enter 1 - 2 only !"
+fi
+
+done
+
